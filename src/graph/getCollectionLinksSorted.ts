f9f292061e42a93289f9946fe35bbf8ad23a988f
Ben Southgate <bsouthga@gmail.com>
2017-09-20 17:33:34 -0400

    fix: partial solution to #48

--- src/graph/getCollectionLinksSorted.ts
+++ src/graph/getCollectionLinksSorted.ts
@@ -12,12 +12,55 @@ export function getCollectionLinksSorted(
   opts: any = { direction: 'outgoing' }
 ): Tyr.FieldInstance[] {
   const collectionFieldCache = plugin._sortedLinkCache,
-        hash = `${col.def.name}:${_.toPairs(opts).map(e => e.join('=')).sort().join(':')}`;
+    hash = `${col.def.name}:${_.toPairs(opts)
+      .map(e => e.join('='))
+      .sort()
+      .join(':')}`;
 
   if (collectionFieldCache[hash]) return collectionFieldCache[hash];
 
-  // sort fields by link collection name
-  const links = _.sortBy(col.links(opts), field => field.link.def.name);
+  const linkFields = col.links(opts);
 
-  return collectionFieldCache[hash] = links;
+  // sort fields by link collection name
+  const links = _.chain(linkFields)
+    .groupBy(field => field.link!.def.name)
+    .map((colLinks: Tyr.FieldInstance[], colName: string) => {
+      /**
+       * multiple links to collection?
+       */
+      if (colLinks.length > 1) {
+        const filtered = colLinks.filter(
+          field =>
+            !!(field.def as Tyr.FieldDefinition & {
+              graclTypes?: string[] | string;
+            }).graclTypes
+        );
+
+        /**
+         * no graclType links? use first available...
+         *
+         * TODO: multiple?
+         */
+        if (filtered.length === 0) {
+          return colLinks.slice(0, 1);
+          /**
+         * one? use it
+         */
+        } else if (filtered.length === 1) {
+          return filtered;
+        } else {
+          throw new Error(
+            `Multiple links to ${colName} for collection ${col.def
+              .name} have graclTypes`
+          );
+        }
+      } else {
+        return colLinks;
+      }
+    })
+    .flatten()
+    .sortBy((field: Tyr.FieldInstance) => field.link && field.link.def.name)
+    .value() as Tyr.FieldInstance[];
+
+  return (collectionFieldCache[hash] = links);
 }
